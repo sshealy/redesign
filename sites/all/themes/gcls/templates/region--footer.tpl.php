367e12b564bb5b9f3e36b8d4ab13ab3fd2964ebb
unknown <sshealy@WEB-DEVELOPER.gclstaff.local>
2015-10-06 13:45:55 -0400

    updating subtheme directory

---
+++ sites/all/themes/gcls/templates/region--footer.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the footer region.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728140
+ */
+?>
+<?php if ($content): ?>
+  <footer id="footer" class="<?php print $classes; ?>">
+    <?php print $content; ?>
+  </footer>
+<?php endif; ?>
+
+<div class="frontpageMenuAreaDiv">
+<div id="mc_embed_signup">
+
+<div class="icons">
+<a href="http://www.facebook.com/greenvillelibrary" target="_blank"><img src="images/face.png"></a>
+<a href="http://twitter.com/GCLSLibrary" target="_blank"><img src="images/twit.png"></a>
+<a href="http://www.youtube.com/user/GreenvilleLibrary" target="_blank"><img src="images/yout.png"></a>
+<a href="http://pinterest.com/gcls/" target="_blank"><img src="images/pin.png"></a>
+<a href="http://instagram.com/greenvillelibrary" target="_blank"><img src="images/insta.png"></a>
+</div>
+
+<div class="nlink">
+<a href="http://eepurl.com/pYSXf" target="_blank"><button class="newsbutton">Get our newsletter</button></a>
+</div>
+</div>
+</div>
