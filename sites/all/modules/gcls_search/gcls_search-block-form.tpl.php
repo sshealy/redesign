1a4b946ba516d97d6e13a0566120fdc2c86a2d87
Sarah <sshealy@greenvillelibrary.org>
2015-11-19 10:07:33 -0500

    backup for custom search

--- sites/all/modules/gcls_search/gcls_search-block-form.tpl.php
+++ sites/all/modules/gcls_search/gcls_search-block-form.tpl.php
@@ -33,5 +33,5 @@
   <?php if (empty($variables['form']['#block']->subject)): ?>
     <h2 class="element-invisible"><?php print t('Search form'); ?></h2>
   <?php endif; ?>
-  <?php print $search_form; ?>
+  <?php print $gcls_search_form; ?>
 </div>
